a53d63151ec70253cdce50e3fbc2bde6c0542307
JamesMelisek <jjm1211@gmail.com>
2022-12-08 02:07:00 -0500

    cleaned it up a bit

--- Day 8/day8_2.py
+++ Day 8/day8_2.py
@@ -1,66 +1,52 @@
+
+#This shouldve just been C++ lol
+
 with open('input.txt', newline='') as csvfile:  
 
     content = csvfile.read().splitlines()
 
-    w, h = 99, 99
-    Matrix = [[0 for x in range(w)] for y in range(h)] 
+    Matrix = [[0 for x in range(99)] for y in range(99)] 
+    scenicScoreMatrix = [[0 for x in range(99)] for y in range(99)] 
 
     for heightCounter, row in enumerate(content):
         for widthCounter, character in enumerate(list(row)):
-
             Matrix[heightCounter][widthCounter] = int(character)
-            print(character, end='')
-
-        print()
-        #print(f' End of the road bucko {heightCounter}')
-
-    for row in Matrix:
-        for column in row:
-            print(column, end='')
-        print()
-
-
-    totalTreesVisible = 0
 
     for rowIdx, row in enumerate(Matrix):
         for colIdx, column in enumerate(row):
 
-            #Check if on the edge
-            if (rowIdx == 0 or colIdx == 0 or rowIdx == 98 or colIdx == 98):
-                totalTreesVisible += 1
-                continue
+            visibleNorth = 0
+            visibleSouth = 0
+            visibleEast = 0
+            visibleWest = 0
 
-            visibleNorth = True
-            visibleSouth = True
-            visibleEast = True
-            visibleWest = True
-
-            #check if anything N
-            for i in range(rowIdx, 0, -1):
-                #print(f'Current tree is: {Matrix[rowIdx][colIdx]} @({rowIdx},{colIdx}), comparing to: {Matrix[i-1][colIdx]}')             
+            for i in range(rowIdx, 0, -1):        
+                visibleNorth += 1           
                 if (Matrix[i-1][colIdx] >= Matrix[rowIdx][colIdx]):
-                    visibleNorth = False
-                    
-            #check if anything S
+                    break
+
             for i in range(rowIdx, 98, 1):
-                #print(f'Current tree is: {Matrix[rowIdx][colIdx]} @({rowIdx},{colIdx}), comparing to: {Matrix[i+1][colIdx]}')             
+                visibleSouth += 1
                 if (Matrix[i+1][colIdx] >= Matrix[rowIdx][colIdx]):
-                    visibleSouth = False
+                    break
 
-            #check if anything W
             for i in range(colIdx, 0, -1):
-                #print(f'Current tree is: {Matrix[rowIdx][colIdx]} @({rowIdx},{colIdx}), comparing to: {Matrix[rowIdx][i-1]}')             
+                visibleWest += 1            
                 if (Matrix[rowIdx][i-1] >= Matrix[rowIdx][colIdx]):
-                    visibleWest = False
+                    break
 
-            #check if anything E
             for i in range(colIdx, 98, 1):
-                #print(f'Current tree is: {Matrix[rowIdx][colIdx]} @({rowIdx},{colIdx}), comparing to: {Matrix[rowIdx][i+1]}')             
+                visibleEast += 1            
                 if (Matrix[rowIdx][i+1] >= Matrix[rowIdx][colIdx]):
-                    visibleEast = False
+                    break
 
-            #print(f'{Matrix[rowIdx][colIdx]}@({rowIdx},{colIdx})  north: {visibleNorth}, south: {visibleSouth}, west: {visibleWest}, east: {visibleEast}')
-            if(visibleNorth or visibleSouth or visibleWest or visibleEast):
-                totalTreesVisible += 1
+            scenicScoreMatrix[rowIdx][colIdx] = visibleNorth * visibleSouth * visibleWest * visibleEast
 
-    print(f'There are {totalTreesVisible} trees visible from the outside')
+    largestScore = 0
+
+    for rowIdx, row in enumerate(scenicScoreMatrix):
+        for colIdx, col in enumerate(row):
+            if scenicScoreMatrix[rowIdx][colIdx] > largestScore:
+                largestScore = scenicScoreMatrix[rowIdx][colIdx]
+
+    print(f'The largest scenic score of any tree is {largestScore}')
