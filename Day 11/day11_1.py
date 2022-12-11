e8d187edacc7cbd3116dee910a8581c71fd50707
JamesMelisek <jjm1211@gmail.com>
2022-12-11 10:55:11 -0500

    half way through monkey one part 1

--- Day 11/day11_1.py
+++ Day 11/day11_1.py
@@ -1,53 +1,105 @@
-class valueToChange:
+class Monkey:
 
-    def __init__(self, increment, cycleToUpdate):
-        self.increment = increment
-        self.cycles = cycleToUpdate
+    def __init__(self, monkeyID, heldItems, monkeyTest, monkeyOperation, ifTruePass, ifFalsePass):
+
+        self.monkeyID = monkeyID
+        self.heldItems = heldItems
+        self.monkeyTest = monkeyTest
+        self.monkeyOperation = monkeyOperation
+        self.ifTruePass = ifTruePass
+        self.ifFalsePass = ifFalsePass
 
 
-with open('input.txt', newline='') as csvfile:  
+    def printInfo(self):
+
+        print()
+        print(f'~~~~ MONKEE #{self.monkeyID} ~~~~')
+        print(self.heldItems)
+        print(self.monkeyTest)
+        print(self.monkeyOperation)
+        print(self.ifTruePass)
+        print(self.ifFalsePass)
+
+
+with open('test.txt', newline='') as csvfile:  
 
     content = csvfile.read().splitlines()
 
+    monkeyNumber = -1
+    itemsToSort = []
+    operation = ''
+    test  =''
+    monkeyToThrowIfTrue = -1
+    monkeyToThrowIfFalse = -1
 
-    registerX = 1
-    cycleCounter = 0
-    signalStrengthSum = 0
+    listOfMonkeys = []
 
-    for row in content:
+    #Populate the monkeys
+    for rowIdx, row in enumerate(content):
         
-        if (row[0:4] == "noop"):     
-            
-            #print('NOOP COMMEND:' + row)
-            cycleCounter += 1
+        if (rowIdx % 7 == 0):     
+            monkeyNumber = row.split(' ')[1][:-1]
 
-            if(cycleCounter == 20 or cycleCounter == 60 or cycleCounter == 100 or cycleCounter == 140 or cycleCounter == 180 or cycleCounter == 220):
-                print(f'CALCULATING {cycleCounter * registerX}')
-                signalStrengthSum += (cycleCounter * registerX)
+        elif (rowIdx % 7 == 1):
+            itemsToSort =  row.split(':')[1].split(',')
 
+            for idx, item in enumerate(itemsToSort):
+                itemsToSort[idx] = int(item[1:])
 
-        elif (row[0:4] == "addx"):
-            
-            increment = int(row.split(' ')[1])
+        elif (rowIdx % 7 == 2):
+            operation = row.split(':')[1][1:]
 
-            cycleCounter += 1
+        elif (rowIdx % 7 == 3):
+            test = row.split(':')[1][1:]
+        
+        elif (rowIdx % 7 == 4):
+            monkeyToThrowIfTrue = int(row.split(' ')[-1])
 
-            if(cycleCounter == 20 or cycleCounter == 60 or cycleCounter == 100 or cycleCounter == 140 or cycleCounter == 180 or cycleCounter == 220):
-                print(f'CALCULATING {cycleCounter * registerX}')
-                signalStrengthSum += (cycleCounter * registerX)
+        elif (rowIdx % 7 == 5):
+            monkeyToThrowIfFalse = int(row.split(' ')[-1])
 
-            cycleCounter += 1
+        elif (rowIdx % 7 == 6):
+            listOfMonkeys.append(Monkey(monkeyNumber, itemsToSort, test, operation, monkeyToThrowIfTrue, monkeyToThrowIfFalse))
+      
+    #for monkee in listOfMonkeys:
+    #    monkee.printInfo()
 
-            if(cycleCounter == 20 or cycleCounter == 60 or cycleCounter == 100 or cycleCounter == 140 or cycleCounter == 180 or cycleCounter == 220):
-                print(f'CALCULATING {cycleCounter * registerX}')
-                signalStrengthSum += (cycleCounter * registerX)
-
-            registerX += increment
-
-            print(f'ADDX COMMEND: {increment} , {row}')
-
-        #print(f'after cycleCounter = {cycleCounter}, registerX = {registerX}')
-        #input()
+    for round in range(1):
 
 
-    print(f'The answer is {signalStrengthSum}')
+        for monkee in listOfMonkeys:
+
+            print(f"MONKEY {monkee.monkeyID}")
+
+            for item in monkee.heldItems:
+
+                testValue = 0
+
+                if (monkee.monkeyOperation.split(' ')[-2] == '*' and monkee.monkeyOperation.split(' ')[-1] == 'old'):
+                    print("hit the exponent monkey!")
+                    testValue = item * item
+
+                elif (monkee.monkeyOperation.split(' ')[-2] == '*'):
+                    testValue = item * int(monkee.monkeyOperation.split(' ')[-1])
+
+                elif (monkee.monkeyOperation.split(' ')[-2] == '+'):   
+                    testValue = item + int(monkee.monkeyOperation.split(' ')[-1])
+
+
+                testValue = testValue // 3
+                print(testValue)
+
+                if (testValue % int(monkee.monkeyTest.split(' ')[-1]) == 0):
+                    print("Divisable!!!")
+                    print(f'Throw to {monkeyToThrowIfTrue}, but located at index {monkeyToThrowIfTrue - 1 }')
+                    listOfMonkeys[monkeyToThrowIfTrue - 1].heldItems.append(item)
+
+                elif (testValue % int(monkee.monkeyTest.split(' ')[-1]) != 0):
+                    print("Not Divisable!!!")
+                    print(f'Throw to {monkeyToThrowIfFalse}, but located at index {monkeyToThrowIfFalse - 1 }')
+                    listOfMonkeys[monkeyToThrowIfFalse - 1].heldItems.append(item)
+
+       
+                input()
+
+    #print(f'The answer is {0}')
